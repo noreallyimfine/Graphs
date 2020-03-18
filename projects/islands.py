bede38d35a67e408117c53d16a8dfb1a8afa80b7
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-03-18 12:33:17 -0400

    classwork islands problem initial commit

---
+++ projects/islands.py
@@ -0,0 +1,17 @@
+# Write a function that takes a 2D binary array and
+# returns the number of 1 islands.
+# An island consists of 1s that are connected to the 
+# north, south, east or west. For example:
+
+islands = [[0, 1, 0, 1, 0],
+           [1, 1, 0, 1, 1],
+           [0, 0, 1, 0, 0],
+           [1, 0, 1, 0, 0],
+           [1, 1, 0, 0, 0]]
+
+# island_counter(islands) -> returns 4
+
+# 1. Translate the problem into graph terminology
+# 2. Build your graph
+# 3. Traverse your graph
+
