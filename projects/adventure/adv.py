72bbf4c9420f9c1340443d0d76fc882310bd8b73
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-03-19 16:32:24 -0400

    fixed some pep8 violations and started feeling around and coming up with a plan. so far looks like im setting up to spaghetti code this but let's see

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -5,19 +5,37 @@ from world import World
 import random
 from ast import literal_eval
 
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.stack)
+
+
 # Load world
 world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
+map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+#map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -30,7 +48,36 @@ player = Player(world.starting_room)
 traversal_path = []
 
 
+## Depth first traversal
+def dft(player):
+    pass
+    s = Stack()
+    # Push starting room to stack
+    s.push(player.current_room)
+    print('player current room:', player.current_room)
+    # intialize empty visited list
+    visited = set()
+    # while stack has stuff in it
+    while s.size() > 0:
+        # pop last element
+        last_room = s.pop()
+        print("Last room", last_room)
+        # check if visited
+        # if not
+        if last_room not in visited:
+            # add to visited
+            visited.add(last_room)
+            print("visited - ", visited)
+            # check every exit
+            for ex in last_room.get_exits():
+                print("exit", ex)
+                # push exit to visited
+                player.travel(ex)
+                s.push(player.current_room)  
+    return visited
 
+
+print(dft(player))
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
@@ -47,16 +94,15 @@ else:
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
-
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
